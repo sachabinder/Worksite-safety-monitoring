c4a0da643f9c06e7d3699b3ba51069d6c2c1a29e
sachabinder <binder.sacha@gmail.com>
2022-09-06 16:01:25 +0200

    env setup

---
+++ environment_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+conda create -n wsm_env python=3.10 -y
+conda activate wsm_env
+
+# TO BE CHANGE WITH YOUR CONFIG
+conda install pytorch torchvision torchaudio cpuonly -c pytorch -y
+conda install -c conda-forge opencv -y
+conda install -c conda-forge matplotlib -y
+conda install -c anaconda pillow -y
+
+
