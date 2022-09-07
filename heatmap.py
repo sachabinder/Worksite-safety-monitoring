1f67acaeff868a58ac9ffb0776847262a938b200
hectormartinet <103176565+hectormartinet@users.noreply.github.com>
2022-09-07 15:35:30 +0200

    hitmap test with one camera angle

--- heatmap.py
+++ heatmap.py
@@ -29,12 +29,12 @@ def test_display(img_path, json_path, class_to_detect="People", line_th=2): # dr
     plt.show()
 
 
-def heatmap(json_path_lst, img_path, class_to_detect="People", coeff=10):
+def heatmap(json_path_lst, img_path, class_to_detect="People", resize_ratio=10):
     img = cv2.imread(img_path)  # Read image with cv2
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
     [w, h] = img.shape[:2]
-    w_heat = w//coeff
-    h_heat = h//coeff
+    w_heat = w//resize_ratio
+    h_heat = h//resize_ratio
     heat_array = np.zeros((w_heat, h_heat))
     for json_path in json_path_lst:
         data = open_json(json_path)
@@ -45,9 +45,8 @@ def heatmap(json_path_lst, img_path, class_to_detect="People", coeff=10):
                 x2 = max(pts[0][0], pts[1][0])
                 y1 = min(pts[0][1], pts[1][1])
                 y2 = max(pts[0][1], pts[1][1])
-                norm = (x2-x1)*(y2-y1)
-                for x in range(x1//coeff, x2//coeff):
-                    for y in range(y1//coeff, y2//coeff):
+                for x in range(x1//resize_ratio, x2//resize_ratio):
+                    for y in range(y1//resize_ratio, y2//resize_ratio):
                         heat_array[y, x] += 1
     df = pd.DataFrame(heat_array, columns=None)
     sn.heatmap(df, xticklabels=False, yticklabels=False, cbar=False)
@@ -57,9 +56,13 @@ def heatmap(json_path_lst, img_path, class_to_detect="People", coeff=10):
     plt.show()
 
 
-json_test_path = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg.json'
+json_test_path = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame3059.jpg.json'
 img_test_path = 'Detection_Train_Set/Detection_Train_Set_Img/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg'
-
+json_begining = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame'
+json_test_path_list = []
+for frame in range(3059, 3540,5):
+    if frame != 3289:
+        json_test_path_list.append(json_begining + str(frame) + ".jpg.json")
 
 #test_display(img_test_path, json_test_path)
-heatmap([json_test_path],img_test_path)
+heatmap(json_test_path_list, img_test_path)
