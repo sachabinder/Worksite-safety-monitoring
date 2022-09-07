a66689ddb6113e7fd56126983451bd96c767a728
sachabinder <binder.sacha@gmail.com>
2022-09-07 22:02:04 +0200

    code refactor

--- model/dataset.py
+++ model/dataset.py
@@ -1,11 +1,13 @@
-from email.mime import image
-from typing import List, Dict
 import torch
-from torchvision.ops import box_convert
 import json
 import pathlib
-from utils import get_filenames_of_path
 import cv2
+from torchvision.ops import box_convert
+from typing import List, Dict
+from utils import (get_filenames_of_path, 
+                   process_image,
+                   get_boxes_and_labels_from_target)
+
 
 IMG_FOLDER = pathlib.Path("Detection_Train_Set/Detection_Train_Set_Img")
 TARGETS_FOLDER = pathlib.Path("Detection_Train_Set/Detection_Train_Set_Json")
@@ -27,12 +29,12 @@ class ObectsDataSet(torch.utils.data.Dataset):
     def __init__(self,
                  image_paths: List[pathlib.Path],
                  target_paths: List[pathlib.Path],
-                 label_index : Dict,
+                 label_indexes: Dict,
                  box_convert_to_format: str = "xywh") -> None:
         self.image_paths = image_paths
         self.target_paths = target_paths
-        self.box_convert_to_format = box_convert_to_format
-        self.label_index = label_index
+        self.box_format_to_convert = box_convert_to_format
+        self.label_indexes = label_indexes
         self.box_format = "xyxy"
 
     def __len__(self):
@@ -41,38 +43,23 @@ class ObectsDataSet(torch.utils.data.Dataset):
         return len(self.image_paths)
     
     def __getitem__(self, index):
-        # image process
-        image = cv2.imread(str(self.image_paths[index]))
-        if len(image.shape) == 4: # convert RGBA -> RGB
-            image = image.convert('RGB')
-        image = torch.from_numpy(image).type(torch.float32)
-        # boxes extraction and process
-        target = json.load(open(self.target_paths[index], "r"))
-        boxes = torch.tensor(
-                    [get_bounding_box(object["points"]["exterior"]) for object in target["objects"]]
-                ).to(torch.float32)
-        boxes = box_convert(boxes, in_fmt=self.box_format, out_fmt=self.box_convert_to_format)
-        # labels extraction
-        labels = torch.tensor(
-                    [LABEL_INDEXES[object["classTitle"]] for object in target["objects"]]
-                 ).to(torch.int64)
-        return {'img':image, 'img_file_name':self.image_paths[index].name, 'target':{'boxes': boxes,'labels': labels}, 'target_file_name':self.target_paths[index].name}
-
-
-def get_bounding_box(poligon_points):
-    """Compute coordinates of bounding box of a polygon.
-    Return points with the format xyxy.
-    (x_1,y_1)-------------------|
-        |                       |
-        |       Polygon         |
-        |                       |
-        ---------------------(x_2,y_2)
-    """
-    x_1 = min([point[0] for point in poligon_points])
-    y_1 = min([point[1] for point in poligon_points])
-    x_2 = max([point[0] for point in poligon_points])
-    y_2 = max([point[1] for point in poligon_points])
-    return [x_1,y_1,x_2,y_2]
+        orig_image, target = self.extract_img_and_target_from_path(img_path=self.image_paths[index],
+                                                                   target_path=self.target_paths[index])
+        image = process_image(image=orig_image)
+        boxes, labels = get_boxes_and_labels_from_target(target=target,
+                                                         box_input_format=self.box_format,
+                                                         box_output_format=self.box_format_to_convert,
+                                                         label_indexes=self.label_indexes)
+        return {'img':image,
+                'img_file_name':self.image_paths[index].name,
+                'target':{'boxes': boxes,'labels': labels},
+                'target_file_name':self.target_paths[index].name}
+    
+    @staticmethod
+    def extract_img_and_target_from_path(img_path, target_path):
+        with open(target_path, "r") as f:
+            target = json.load(f)
+        return cv2.imread(str(img_path)), target
 
 
 if __name__ == "__main__":
@@ -81,5 +68,5 @@ if __name__ == "__main__":
     targets.sort()
     images.sort()
 
-    data_set = ObectsDataSet(images, targets)
+    data_set = ObectsDataSet(images, targets,LABEL_INDEXES)
     print(data_set[0])
--- model/utils.py
+++ model/utils.py
@@ -1,20 +1,57 @@
-import json
-import os
 import pathlib
-from typing import List, Union
+import torch
+import json
+import cv2
+import numpy as np
+from torchvision.ops import box_convert
+from typing import List, Tuple, Dict
 
 
 def get_filenames_of_path(path: pathlib.Path, ext: str = "*") -> List[pathlib.Path]:
-    """
-    Returns a list of files in a directory/path. Uses pathlib.
+    """Returns a list of files in a directory/path. Uses pathlib.
     """
     filenames = [file for file in path.glob(ext) if file.is_file()]
     assert len(filenames) > 0, f"No files found in path: {path}"
     return filenames
 
 
-def read_json(path: pathlib.Path) -> dict:
-    with open(str(path), "r") as fp:  # fp is the file pointer
-        file = json.loads(s=fp.read())
+#=========== DATA SET AUXILIARY FUNCTIONS ===========
 
-    return file
+def process_image(image:np.array) -> torch.Tensor:
+    """Return a torch tensor image in RGB loaded from a path.
+    """
+    if len(image.shape) == 4: # convert RGBA -> RGB
+        image = image.convert('RGB')
+    return torch.from_numpy(image).type(torch.float32)
+
+def get_boxes_and_labels_from_target(target:Dict,
+                                     box_input_format:str,
+                                     box_output_format:str,
+                                     label_indexes:Dict) -> Tuple[torch.Tensor]:
+    """Return a torch tensor couple of (boxes, labels), 
+    convert format : box_input_format -> box_output_format,
+    from a polygon loaded through JSON file.
+    """
+    boxes = torch.tensor(
+                [get_bounding_box(object["points"]["exterior"]) for object in target["objects"]]
+            ).to(torch.float32)
+    boxes_converted = box_convert(boxes, in_fmt=box_input_format, out_fmt=box_output_format)
+    labels = torch.tensor(
+                    [label_indexes[object["classTitle"]] for object in target["objects"]]
+                 ).to(torch.int64)
+    return (boxes_converted, labels)
+
+def get_bounding_box(poligon_points):
+    """Compute coordinates of bounding box of a polygon.
+    Return points with the format xyxy.
+    (x_1,y_1)-------------------|
+        |                       |
+        |       Polygon         |
+        |                       |
+        ---------------------(x_2,y_2)
+    """
+    x_1 = min([point[0] for point in poligon_points])
+    y_1 = min([point[1] for point in poligon_points])
+    x_2 = max([point[0] for point in poligon_points])
+    y_2 = max([point[1] for point in poligon_points])
+    return [x_1,y_1,x_2,y_2]
