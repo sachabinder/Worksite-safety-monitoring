eb34592152baf9d38f530efc74d1200e8430870b
hectormartinet <103176565+hectormartinet@users.noreply.github.com>
2022-09-08 12:28:35 +0200

    heatmap with img blend in

--- heatmap.py
+++ heatmap.py
@@ -10,49 +10,49 @@ import os
 
 
 #=================================== Image diffenrentiation
-def differenceImage(image1, image2):
+def difference_image(image1, image2):
     tab1 = np.asarray(image1)
     tab2 = np.asarray(image2)
     return Image.fromarray(tab1 - tab2)
 
 
-def normImage(img):
+def norm_image(img):
     tab = np.asarray(img)
     return np.linalg.norm(tab, ord=1)
 
 
-def gaussienne(x):
+def gaussian(x):
     return 255 * np.exp(-((x - 127.5) / 30) ** 2)
 
 
-def isSameCam(img1, img2, threshold):
+def is_same_cam(img1, img2, threshold):
     if img1.size == img2.size:
-        I = differenceImage(img1, img2)
-        I = Image.eval(I, gaussienne)
-        return normImage(I) < threshold
+        I = difference_image(img1, img2)
+        I = Image.eval(I, gaussian)
+        return norm_image(I) < threshold
     else:
         return False
 
-# -----------
-def imageDifferentiation(threshold, path):
-    ImageNameList = os.listdir(path)
-    CameraList = []
 
-    for imageName in ImageNameList:
+def image_differentiation(threshold, path):
+    image_name_list = os.listdir(path)
+    camera_list = []
+
+    for imageName in image_name_list:
         img1 = Image.open(path + "/" + imageName)
         img1 = img1.convert('L')  # Greyscale conversion
-        NewCamNecessary = True
-        for camera in CameraList:
+        new_cam_necessary = True
+        for camera in camera_list:
             img2 = Image.open(path + "/" + camera[0])
             img2 = img2.convert('L')
-            if isSameCam(img1, img2, threshold):
+            if is_same_cam(img1, img2, threshold):
                 camera.append(imageName)
-                NewCamNecessary = False
+                new_cam_necessary = False
 
-        if NewCamNecessary:
-            CameraList.append([imageName])
+        if new_cam_necessary:
+            camera_list.append([imageName])
 
-    return CameraList
+    return camera_list
 
 
 
@@ -112,38 +112,50 @@ def display_detection(img_path, json_path, class_to_detect="People", line_th=2):
     plt.show()
 
 
-def heatmap(json_path_lst, img_path, class_to_detect="People", resize_ratio=3):
+def heatmap(json_path_lst, img_path, class_to_detect="People"):
     img = cv2.imread(img_path)  # Read image with cv2
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # Convert to RGB
     [w, h] = img.shape[:2]
-    heat_array = np.zeros((w//resize_ratio, h//resize_ratio))
+    heat_array = np.zeros((w, h))
 
     for json_path in json_path_lst:
         boxes = get_bounding_box(json_path, class_to_detect)
         for [[x1, y1], [x2, y2]] in boxes:
-            for x in range(x1//resize_ratio, x2//resize_ratio):
-                for y in range(y1//resize_ratio, y2//resize_ratio):
+            for x in range(x1, x2):
+                for y in range(y1, y2):
                     heat_array[y, x] += 1
 
-    df = pd.DataFrame(heat_array, columns=None)
-    sn.heatmap(df, xticklabels=False, yticklabels=False, cbar=False)
-    plt.pcolor(df)
-    plt.show()
+    heat_array = np.floor(255*heat_array/np.max(heat_array))
+    img = Image.open(img_path)
+    heat_img = Image.fromarray(heat_array)
+    heat_img = heat_img.convert('RGB')
+    new_image = Image.blend(heat_img, img, .25)
+    new_image.show()
 
 
-if __name__ =="__main__":
-    json_test_path = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame3059.jpg.json'
-    img_test_path = 'Detection_Train_Set/Detection_Train_Set_Img/Batch2__BioSAV_BIofiltration_18mois_05frame3059.jpg'
+
+if __name__ == "__main__":
+    '''
+    path_img = "Detection_Test_Set/Detection_Test_Set_Img"
+    path_json = "Detection_Test_Set/Detection_Test_Set_Json"
+    camera_lst = image_differentiation(50000, path_img)
+    
+    for camera in camera_lst:
+        json_path_list = [path_json + "/" + link + ".json" for link in camera]
+        heatmap(json_path_list, path_img + "/" + camera[0])
+    '''
+    json_test_path = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg.json'
+    img_test_path = 'Detection_Train_Set/Detection_Train_Set_Img/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg'
     json_begining = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame'
-
+    
     json_test_path_list = []
-    for frame in range(3059, 3540, 5):
-        if frame != 3289:
+    for frame in range(3049, 3540, 5):
+        if frame != 3289 and frame != 3054:
             json_test_path_list.append(json_begining + str(frame) + ".jpg.json")
 
-    display_detection(img_test_path, json_test_path)
+    # display_detection(img_test_path, json_test_path)
     heatmap(json_test_path_list, img_test_path)
+
   
     #----------
-    imageDifferentiation(50000, "Detection_Test_Set/Detection_Test_Set_Img")
+
     
