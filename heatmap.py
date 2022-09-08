a1af2352235550a4d71096496c86c2f94453d101
hectormartinet <103176565+hectormartinet@users.noreply.github.com>
2022-09-08 14:44:28 +0200

    red color added + separation of images for train set

--- heatmap.py
+++ heatmap.py
@@ -34,6 +34,29 @@ def is_same_cam(img1, img2, threshold):
         return False
 
 
+def similar_name(img_name1, img_name2):
+    test_length = 15
+    return img_name1[:test_length] == img_name2[:test_length]
+
+
+def sort_images_names(path):
+    image_name_list = os.listdir(path)
+    camera_list = []
+    for img_name1 in image_name_list:
+        new_cam_necessary = True
+        for camera in camera_list:
+            img_name2 = camera[0]
+            if similar_name(img_name1, img_name2):
+                camera.append(img_name1)
+                new_cam_necessary = False
+                break
+
+        if new_cam_necessary:
+            camera_list.append([img_name1])
+
+    return camera_list
+
+
 def image_differentiation(threshold, path):
     image_name_list = os.listdir(path)
     camera_list = []
@@ -115,34 +138,35 @@ def display_detection(img_path, json_path, class_to_detect="People", line_th=2):
 def heatmap(json_path_lst, img_path, class_to_detect="People"):
     img = cv2.imread(img_path)  # Read image with cv2
     [w, h] = img.shape[:2]
-    heat_array = np.zeros((w, h))
+    heat_array = np.zeros((w, h, 3))
 
     for json_path in json_path_lst:
         boxes = get_bounding_box(json_path, class_to_detect)
         for [[x1, y1], [x2, y2]] in boxes:
             for x in range(x1, x2):
                 for y in range(y1, y2):
-                    heat_array[y, x] += 1
+                    heat_array[y, x, 0] += 1
 
-    heat_array = np.floor(255*heat_array/np.max(heat_array))
+    heat_array = np.array(255*heat_array/np.max(heat_array), np.int8)
+    heat_img = Image.fromarray(heat_array, mode='RGB')
     img = Image.open(img_path)
-    heat_img = Image.fromarray(heat_array)
-    heat_img = heat_img.convert('RGB')
     new_image = Image.blend(heat_img, img, .25)
     new_image.show()
 
 
 
 if __name__ == "__main__":
-    '''
-    path_img = "Detection_Test_Set/Detection_Test_Set_Img"
-    path_json = "Detection_Test_Set/Detection_Test_Set_Json"
-    camera_lst = image_differentiation(50000, path_img)
-    
+
+    path_img_train = "Detection_Train_Set/Detection_Train_Set_Img"
+    path_json_train = "Detection_Train_Set/Detection_Train_Set_Json"
+    path_img_test = "Detection_Test_Set/Detection_Test_Set_Img"
+    path_json_test = "Detection_Test_Set/Detection_Test_Set_Json"
+    #camera_lst = image_differentiation(50000, path_img)
+    camera_lst = sort_images_names(path_img_train)
     for camera in camera_lst:
-        json_path_list = [path_json + "/" + link + ".json" for link in camera]
-        heatmap(json_path_list, path_img + "/" + camera[0])
-    '''
+        json_path_list = [path_json_train + "/" + link + ".json" for link in camera]
+        heatmap(json_path_list, path_img_train + "/" + camera[0])
+
     json_test_path = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg.json'
     img_test_path = 'Detection_Train_Set/Detection_Train_Set_Img/Batch2__BioSAV_BIofiltration_18mois_05frame3049.jpg'
     json_begining = 'Detection_Train_Set/Detection_Train_Set_Json/Batch2__BioSAV_BIofiltration_18mois_05frame'
@@ -154,8 +178,3 @@ if __name__ == "__main__":
 
     # display_detection(img_test_path, json_test_path)
     heatmap(json_test_path_list, img_test_path)
-
-  
-    #----------
-
-    
