a4de1e40bcc8855cf5fa08d0e24c0e9faf269052
Arnaud <arnaud.virelizier@eleves.enpc.fr>
2022-09-09 12:39:47 +0200

    clean coded heatmap

--- heatmap.py
+++ heatmap.py
@@ -4,59 +4,61 @@ import os
 import matplotlib.pyplot as plt
 
 
-def differenceImage(image1, image2):
+def difference_image(image1, image2):
     tab1 = np.asarray(image1)
     tab2 = np.asarray(image2)
     return Image.fromarray(tab1 - tab2)
 
 
-def normImage(img):
+def norm_image(img):
     tab = np.asarray(img)
     return np.linalg.norm(tab, ord=1)
 
 
-def gaussienne(x):
+def gaussian(x):
     return 255 * np.exp(-((x - 127.5) / 30) ** 2)
 
 
-def isSameCam(img1, img2, threshold):
+def is_same_cam(img1, img2, threshold):
     if img1.size == img2.size:
-        I = differenceImage(img1, img2)
-        I = Image.eval(I, gaussienne)
-        return normImage(I) < threshold
+        im = difference_image(img1, img2)
+        im = Image.eval(im, gaussian)
+        return norm_image(im) < threshold
     else:
         return False
 
 
-def imageDifferentiation(threshold, path):
-    ImageNameList = os.listdir(path)
-    CameraList = []
+def image_differentiation(threshold, path):
+    camera_list = []
+    image_name_list = os.listdir(path)
 
-    for imageName in ImageNameList:
-        img1 = Image.open(path + imageName).convert('L') # Opening and greyscale conversion
-        NewCamNecessary = True
+    for image_name in image_name_list:
+        img1 = Image.open(path + image_name).convert('L')  # Opening and greyscale conversion
+        new_cam_necessary = True
 
-        for camera in CameraList:
+        for camera in camera_list:
             img2 = Image.open(path + camera[0]).convert('L')
-            if isSameCam(img1, img2, threshold):
-                camera.append(imageName)
-                NewCamNecessary = False
+            if is_same_cam(img1, img2, threshold):
+                camera.append(image_name)
+                new_cam_necessary = False
 
-        if NewCamNecessary:
-            CameraList.append([imageName])
+        if new_cam_necessary:
+            camera_list.append([image_name])
 
-    return CameraList
+    return camera_list
 
 
 if __name__ == "__main__":
     path = "Detection_Test_Set/Detection_Test_Set_Img/"
-    CameraList = imageDifferentiation(60000, path)
+    threshold = 55000
+
+    CameraList = image_differentiation(threshold, path)
     for camera in CameraList:
         k = 1
         for image in camera:
             img = Image.open(path + image)
-            if k < 17:
-                plt.subplot(4, 4, k)
+            if k < 25:
+                plt.subplot(5, 5, k)
                 plt.imshow(img)
             k += 1
 
